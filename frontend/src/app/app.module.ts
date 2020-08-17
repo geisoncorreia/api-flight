e7e70dbd6c55a7675eba68f2543e08ef536979de
Geison Correia <geisonthcorreia@gmail.com>
2020-08-17 18:42:20 -0300

    Criando o service, components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PriceComponent } from './components/price/price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
